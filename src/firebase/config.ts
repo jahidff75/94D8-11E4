2fb1e2f779001bf54c77cbf90e14e9a8c3e95bca
GAMING JAHID <jahiduls297@gmail.com>
2025-11-10 10:12:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3862919807-c536f",
+  "appId": "1:450078527325:web:ce0a27f6ac810d682462b4",
+  "apiKey": "AIzaSyBopab46F-SvOeULO5i6Z83kBTuNHwDPQk",
+  "authDomain": "studio-3862919807-c536f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "450078527325"
+};
