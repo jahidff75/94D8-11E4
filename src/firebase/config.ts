601d06785013674ec34e0477e37e21cdb13e7399
GAMING JAHID <jahiduls297@gmail.com>
2025-11-10 17:19:03 +0000

    const firebaseConfig = {   apiKey: "AIzaSyA_XEddYD4WzwLDZM_uIgNB9cHiZai3

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBopab46F-SvOeULO5i6Z83kBTuNHwDPQk",
-  authDomain: "studio-3862919807-c536f.firebaseapp.com",
-  projectId: "studio-3862919807-c536f",
-  storageBucket: "studio-3862919807-c536f.firebasestorage.app",
-  messagingSenderId: "450078527325",
-  appId: "1:450078527325:web:f29efe5c0b04e5272462b4"
+  apiKey: "AIzaSyA_XEddYD4WzwLDZM_uIgNB9cHiZai3StY",
+  authDomain: "jahids-devlop.firebaseapp.com",
+  projectId: "jahids-devlop",
+  storageBucket: "jahids-devlop.firebasestorage.app",
+  messagingSenderId: "625970309302",
+  appId: "1:625970309302:web:1ed031ddc902bddb469e17"
 };
