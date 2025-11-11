bac17732bc9a77192d1d39b53b698b35dda51438
GAMING JAHID <jahiduls297@gmail.com>
2025-11-11 01:50:31 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA_XEddYD4WzwLDZM_uIgNB9cHiZai3StY",
-  authDomain: "jahids-devlop.firebaseapp.com",
-  databaseURL: "https://jahids-devlop-default-rtdb.firebaseio.com",
-  projectId: "jahids-devlop",
-  storageBucket: "jahids-devlop.firebasestorage.app",
-  messagingSenderId: "625970309302",
-  appId: "1:625970309302:web:1ed031ddc902bddb469e17",
-  measurementId: "G-4460NFM6YB"
+  apiKey: "AIzaSyA1d32ux87dwz12ujfDOttiWjLGzucCpl0",
+  authDomain: "jahid-ltd.firebaseapp.com",
+  databaseURL: "https://jahid-ltd-default-rtdb.firebaseio.com",
+  projectId: "jahid-ltd",
+  storageBucket: "jahid-ltd.firebasestorage.app",
+  messagingSenderId: "382996631305",
+  appId: "1:382996631305:web:386ca1a32330f0b4240dd8",
+  measurementId: "G-16Z522WPJL"
 };
